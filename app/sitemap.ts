d67262a7db2d566c1bb832ef3a47df14a0ec4bc7
armanyaempire-sketch <armanyaempire@gmail.com>
2026-09-22 00:42:46 +0530

    Add ProDentim product page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,7 @@ export default function sitemap():MetadataRoute.Sitemap{
    return{url:SITE_URL+"/category/"+c.slug+"/",lastModified};
   }),
   ...articles.map(a=>({url:SITE_URL+"/articles/"+a.slug+"/",lastModified:a.updated})),
+  {url:SITE_URL+"/products/prodentim/",lastModified:EDITORIAL_REVIEW_ISO},
   ...trustPages.map(p=>({url:SITE_URL+"/"+p+"/",lastModified:EDITORIAL_REVIEW_ISO}))
  ];
 }
